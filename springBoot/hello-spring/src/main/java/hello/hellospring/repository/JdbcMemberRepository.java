db288080ee0f3c3ad39992808d8571b1d21ba631
soowan95 <ksw9565@gmail.com>
2023-09-07 22:47:07 +0900

    fix whitelebal error

--- springBoot/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ springBoot/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,4 +1,5 @@
 package hello.hellospring.repository;
+
 import hello.hellospring.domain.Member;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import javax.sql.DataSource;
@@ -6,24 +7,31 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 public class JdbcMemberRepository implements MemberRepository {
+
   private final DataSource dataSource;
   public JdbcMemberRepository(DataSource dataSource) {
     this.dataSource = dataSource;
   }
+
   @Override
   public Member save(Member member) {
     String sql = "insert into member(name) values(?)";
+
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
+
     try {
       conn = getConnection();
-      pstmt = conn.prepareStatement(sql,
-              Statement.RETURN_GENERATED_KEYS);
+      pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
       pstmt.setString(1, member.getName());
+
       pstmt.executeUpdate();
       rs = pstmt.getGeneratedKeys();
+
       if (rs.next()) {
         member.setId(rs.getLong(1));
       } else {
@@ -36,17 +44,22 @@ public class JdbcMemberRepository implements MemberRepository {
       close(conn, pstmt, rs);
     }
   }
+
   @Override
   public Optional<Member> findById(Long id) {
     String sql = "select * from member where id = ?";
+
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
+
     try {
       conn = getConnection();
       pstmt = conn.prepareStatement(sql);
       pstmt.setLong(1, id);
+
       rs = pstmt.executeQuery();
+
       if(rs.next()) {
         Member member = new Member();
         member.setId(rs.getLong("id"));
@@ -61,16 +74,21 @@ public class JdbcMemberRepository implements MemberRepository {
       close(conn, pstmt, rs);
     }
   }
+
   @Override
   public List<Member> findAll() {
     String sql = "select * from member";
+
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
+
     try {
       conn = getConnection();
       pstmt = conn.prepareStatement(sql);
+
       rs = pstmt.executeQuery();
+
       List<Member> members = new ArrayList<>();
       while(rs.next()) {
         Member member = new Member();
@@ -85,15 +103,19 @@ public class JdbcMemberRepository implements MemberRepository {
       close(conn, pstmt, rs);
     }
   }
+
   @Override
   public Optional<Member> findByName(String name) {
     String sql = "select * from member where name = ?";
+
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
+
     try {
       conn = getConnection();
       pstmt = conn.prepareStatement(sql);
+
       pstmt.setString(1, name);
       rs = pstmt.executeQuery();
       if(rs.next()) {
@@ -109,11 +131,12 @@ public class JdbcMemberRepository implements MemberRepository {
       close(conn, pstmt, rs);
     }
   }
+
   private Connection getConnection() {
     return DataSourceUtils.getConnection(dataSource);
   }
-  private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
-  {
+
+  private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
     try {
       if (rs != null) {
         rs.close();
@@ -136,6 +159,7 @@ public class JdbcMemberRepository implements MemberRepository {
       e.printStackTrace();
     }
   }
+
   private void close(Connection conn) throws SQLException {
     DataSourceUtils.releaseConnection(conn, dataSource);
   }
